373828ece62e89774a86fe133b49b01b4acce41d
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2024-12-04 15:24:29 -0600

    set vscode as true always

--- config/init.lua
+++ config/init.lua
@@ -33,7 +33,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup('custom.plugins', {
   defaults = {
-    cond = vim.g.vscode
+    cond = true
   },
   checker = {
     enabled = true,
