a3432aa2f9286f018ef5f1aca8108addd9df960e
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-09-17 17:09:18 -0500

    added devlopment and media packages

---
+++ modules/shared/packages/media.nix
@@ -0,0 +1,21 @@
+# Media and graphics packages
+{ pkgs }:
+
+with pkgs; [
+  # Graphics and media libraries
+  cairo
+  adwaita-icon-theme
+  gtk3
+  gtksourceview
+  libxml2
+
+  # Build dependencies for media apps
+  expat
+  gettext
+  gnupatch
+  parallel
+  rlwrap
+  ccache
+  libgit2
+  zstd
+]
