49442df338983a65af23f9b6c5b8608f4491f75b
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-09-17 16:38:56 -0500

    added default for short config programs and wrapper

---
+++ modules/shared/programs/default.nix
@@ -0,0 +1,35 @@
+{ ... }:
+{
+  imports = [
+    ./git.nix
+    ./neovim
+    ./vscode.nix
+    ./tmux.nix
+    ./zsh.nix
+  ];
+
+  programs.atuin = {
+    enable = true;
+    flags = [ "--disable-up-arrow" ];
+  };
+  programs.bat.enable = true;
+  programs.btop.enable = true;
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+  programs.eza = {
+    enable = true;
+    icons = "auto";
+  };
+  programs.fd.enable = true;
+  programs.fzf.enable = true;
+  programs.lazygit.enable = true;
+  programs.nh = {
+    enable = true;
+  };
+  programs.ripgrep.enable = true;
+  programs.vim.enable = true;
+  programs.zoxide.enable = true;
+}
