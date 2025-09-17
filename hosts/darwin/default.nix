a1ece71cade9b28fdea57f411a9796cc54a999e1
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-09-17 16:27:08 -0500

    added default and amcbookair basic config

---
+++ hosts/darwin/default.nix
@@ -0,0 +1,7 @@
+# Legacy default.nix - use common.nix + specific configs instead
+{ ... }: {
+  imports = [
+    ./common.nix
+    ./macbook-air.nix # Default to macbook-air for backward compatibility
+  ];
+}
