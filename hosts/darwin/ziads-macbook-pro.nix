b52a23d0de5de4e8e7a61fcccf375ba8626a09b3
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-04-12 15:30:08 -0500

    added files for pro

--- hosts/darwin/ziads-macbook-pro.nix
+++ hosts/darwin/ziads-macbook-pro.nix
@@ -1,4 +1,4 @@
-{ hostname, ... }:
+{ hostname, lib, ... }:
 {
   networking.computerName = "Ziad's MacBook Pro";
   networking.hostName = hostname;
@@ -7,5 +7,7 @@
     defaults.smb.NetBIOSName = hostname;
     stateVersion = 5;
   };
+  nix.enable = lib.mkForce false;
+  nix.gc.automatic = lib.mkForce false;
 
 }
